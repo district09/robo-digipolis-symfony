46711b1c4cc760578ce74fa0a8328308ad81cf55
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-03-28 15:13:57 +0200

    Add missing use statement.

--- src/RoboFileBase.php
+++ src/RoboFileBase.php
@@ -3,6 +3,7 @@
 namespace DigipolisGent\Robo\Symfony;
 
 use DigipolisGent\Robo\Helpers\AbstractRoboFile;
+use DigipolisGent\Robo\Task\Deploy\Ssh\Auth\AbstractAuth;
 use DigipolisGent\Robo\Task\Deploy\Ssh\Auth\KeyFile;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
