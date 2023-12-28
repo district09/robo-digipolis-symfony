d2f020c96024aa491fa29abd08ba19e9bfd65432
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2023-12-28 13:23:29 +0100

    Implement correct interfaces for automatic injection

--- src/EventHandler/PreRestoreBackupRemoteHandler.php
+++ src/EventHandler/PreRestoreBackupRemoteHandler.php
@@ -2,14 +2,17 @@
 
 namespace DigipolisGent\Robo\Symfony\EventHandler;
 
+use Consolidation\AnnotatedCommand\Events\CustomEventAwareInterface;
 use DigipolisGent\Robo\Helpers\EventHandler\DefaultHandler\PreRestoreBackupRemoteHandler as PreRestoreBackupRemoteHandlerBase;
 use DigipolisGent\Robo\Helpers\Util\RemoteConfig;
 use DigipolisGent\Robo\Task\Deploy\Ssh\Auth\KeyFile;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
-class PreRestoreBackupRemoteHandler extends PreRestoreBackupRemoteHandlerBase
+class PreRestoreBackupRemoteHandler extends PreRestoreBackupRemoteHandlerBase implements CustomEventAwareInterface
 {
     use \DigipolisGent\Robo\Symfony\Util\SymfonyUtil;
+    use \DigipolisGent\Robo\Helpers\Traits\EventDispatcher;
+    use \Consolidation\AnnotatedCommand\Events\CustomEventAwareTrait;
 
     public function getPriority(): int
     {
--- src/EventHandler/SymfonyHandler.php
+++ src/EventHandler/SymfonyHandler.php
@@ -2,9 +2,12 @@
 
 namespace DigipolisGent\Robo\Symfony\EventHandler;
 
+use Consolidation\AnnotatedCommand\Events\CustomEventAwareInterface;
 use DigipolisGent\Robo\Helpers\EventHandler\AbstractTaskEventHandler;
 
-abstract class SymfonyHandler extends AbstractTaskEventHandler
+abstract class SymfonyHandler extends AbstractTaskEventHandler implements CustomEventAwareInterface
 {
     use \DigipolisGent\Robo\Symfony\Util\SymfonyUtil;
+    use \DigipolisGent\Robo\Helpers\Traits\EventDispatcher;
+    use \Consolidation\AnnotatedCommand\Events\CustomEventAwareTrait;
 }
